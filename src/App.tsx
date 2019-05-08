34677cb31f509f486fb4affda32b95d259cfb500
任梁樊 <renliangfan@192.168.101.105>
2019-05-08 16:49:21 +0800

    feat: project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import JForm from './components/j-form';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <JForm/>
+    </div>
+  );
+};
+
+export default App;
